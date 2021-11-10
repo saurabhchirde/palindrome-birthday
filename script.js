004499d286a546cca30aaf90984bbc6694c3a6b3
saurabhchirde <saurabh123.chirde@gmail.com>
2021-11-10 17:22:07 +0530

    dateVariations

--- script.js
+++ script.js
@@ -13,6 +13,39 @@ function checkPalindrome(str) {
   return str === reverseText;
 }
 
-console.log(checkPalindrome("saurabh"));
-console.log(checkPalindrome("oppo"));
-console.log(checkPalindrome("racecar"));
+function dateToString(date) {
+  var dateStr = { day: "", month: "", year: "" };
+  if (date.day < 10) {
+    dateStr.day = "0" + date.day;
+  } else {
+    dateStr.day = date.day.toString();
+  }
+
+  if (date.month < 10) {
+    dateStr.month = "0" + date.month;
+  } else {
+    dateStr.month = date.month.toString();
+  }
+  dateStr.year = date.year.toString();
+  return dateStr;
+}
+
+var date = {
+  day: 24,
+  month: 9,
+  year: 2020,
+};
+
+function dateVariations(date) {
+  var dateStr = dateToString(date);
+
+  var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+  var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+  var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
+  var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+
+console.log(dateVariations(date));
