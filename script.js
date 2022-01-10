a04473e36104370b36b27e624f360aa0bf8497ef
saurabhchirde <saurabh123.chirde@gmail.com>
2022-01-10 23:53:15 +0530

    fix: func decl to arrow fun

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const btnCheck = document.querySelector(".btnCheck");
 
 const output = document.querySelector(".output");
 
-function reverseStr(str) {
+const reverseStr = (str) => {
   const reversed = str.split("").reverse().join("");
   return reversed;
-}
+};
 
-function checkPalindrome(str) {
+const checkPalindrome = (str) => {
   const reverseText = reverseStr(str);
   return str === reverseText;
-}
+};
 
-function dateToString(date) {
+const dateToString = (date) => {
   var dateStr = { day: "", month: "", year: "" };
   if (date.day < 10) {
     dateStr.day = "0" + date.day;
@@ -28,9 +28,9 @@ function dateToString(date) {
   }
   dateStr.year = date.year.toString();
   return dateStr;
-}
+};
 
-function dateVariations(date) {
+const dateVariations = (date) => {
   var dateStr = dateToString(date);
 
   var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
@@ -40,9 +40,9 @@ function dateVariations(date) {
   var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2);
   var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day;
   return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
-}
+};
 
-function isPalindromeAllDateVariations(date) {
+const isPalindromeAllDateVariations = (date) => {
   const dateArray = dateVariations(date);
   var flag = false;
   for (let i = 0; i < dateArray.length; i++) {
@@ -52,10 +52,10 @@ function isPalindromeAllDateVariations(date) {
     }
   }
   return flag;
-}
+};
 
 //leap year function
-function leapYear(year) {
+const leapYear = (year) => {
   if (year % 400 === 0) {
     return true;
   }
@@ -66,10 +66,10 @@ function leapYear(year) {
     return true;
   }
   return false;
-}
+};
 
 //next date
-function nextDate(date) {
+const nextDate = (date) => {
   var day = date.day + 1;
   var month = date.month;
   var year = date.year;
@@ -105,10 +105,10 @@ function nextDate(date) {
     month: month,
     year: year,
   };
-}
+};
 
 //check next palindrome
-function nextDatePalindrome(date) {
+const nextDatePalindrome = (date) => {
   var cnt = 0;
   var nxtDate = nextDate(date);
   while (1) {
@@ -120,9 +120,9 @@ function nextDatePalindrome(date) {
     nxtDate = nextDate(nxtDate);
   }
   return [cnt, nxtDate];
-}
+};
 
-function clickHandler() {
+const clickHandler = () => {
   var input = inputText.value;
   if (input !== "") {
     var listDate = input.split("-");
@@ -141,6 +141,6 @@ function clickHandler() {
   } else {
     output.innerText = "enter or select a valid date ";
   }
-}
+};
 
 btnCheck.addEventListener("click", clickHandler);
